d98bebe0acc5f7b613b5b29c37332719a1610b18
Hadiyan <hadi.rafi0324@gmail.com>
2021-04-26 04:47:30 +0700

    siswa model

--- app/Models/Siswa.php
+++ app/Models/Siswa.php
@@ -4,13 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Siswa extends Model
+class Siswa extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
 
     protected $table = 'siswa';
-    protected $guarded = ['id'];
+    protected $guard = 'admin';
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 
     public function kelas()
     {
